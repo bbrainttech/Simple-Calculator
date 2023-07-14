6fd9aa2a2d6963f6ebc7257ae960046aa233b085
braintech <120840671+bbrainttech@users.noreply.github.com>
2023-07-14 17:40:16 +0100

    Almost Done!

--- script.js
+++ script.js
@@ -3,17 +3,28 @@ const userInp = document.querySelector('.inp input'),
     clear = document.getElementById("clear"),
     compute = document.querySelector('.compute'),
     clearAll = document.getElementById("clearAll"),
+    themeTog = document.querySelectorAll('.theme-toggle>i'),
     togPlusMinus = document.getElementById('togPlusMinus'),
     allNums = document.querySelectorAll('.board>div:not(.spc,.compute,#clearAll,#togPlusMinus,#per,#root,.hist)'),
     allOps = document.querySelectorAll('.ops');
 
 (localStorage.result && localStorage.expression) ? (() => { userInp.value = localStorage.expression; setTimeout(() => { result.classList.add('hasData'); result.innerHTML = localStorage.result }, 200) })() : null
 
-
+themeTog.forEach(theme => {
+    theme.onmousedown = function () {
+        
+        themeTog[0].classList.toggle('act')
+        themeTog[1].classList.toggle('act')
+        if(this.dataset.value === 'light'){
+            document.body.setAttribute('data-theme','light')} else{
+            document.body.setAttribute('data-theme','dark') }
+    }
+})
+// document.documentElement.style.setProperty('--sub1','red')
 allNums.forEach(num => {
     num.onmousedown = function () {
         userInp.value += this.innerHTML
-        // computeExp(userInp.value)
+        computeExp(userInp.value)
     }
 })
 
@@ -22,7 +33,7 @@ function updateStorage(f) {
     localStorage.result = result.innerHTML
 }
 upDateSize = (i) => {
-    let l = result.textContent.length;
+    let l = i.textContent.length;
     (l > 7) ? i.classList.add('b1') : (l > 14) ? i.classList.add('b2') : i.classList.add('b3')
 }
 
@@ -36,18 +47,26 @@ clearAll.onmousedown = () => {
     updateStorage(userInp.value)
 }
 togPlusMinus.onclick = () => {
-    (userInp.value.split('').includes('–')) ? -1 * Number(computeExp(userInp.value)) : console.log('no')
+    // (userInp.value.split('').includes('–')) ? computeExp(userInp.value): computeExp(userInp.value)
+}
+const replaceIn = i => {
+    return i.replaceAll('-', ' – ')
+        .replaceAll('*', ' × ')
+        .replaceAll('/', ' ∻ ')
+        .replaceAll(' ', '')
+}
+const replaceOut = o => {
+    return o.replaceAll('–', '-')
+        .replaceAll('×', '*')
+        .replaceAll('∻', '/')
+        .replaceAll('^', '**')
+        .replaceAll(' ', '')
 }
 const computeExp = (f) => {
     if (!f) return
-    userInp.value = userInp.value.toLocaleString()
+    userInp.value = replaceIn(userInp.value)
     result.classList.add('hasData')
-    result.innerHTML = eval(
-        f.replaceAll('–', '-')
-            .replaceAll('×', '*')
-            .replaceAll('∻', '/')
-            .replaceAll(' ', '')
-    ).toLocaleString()
+    result.innerHTML = eval(replaceOut(f)).toLocaleString()
     updateStorage(f)
 }
 
